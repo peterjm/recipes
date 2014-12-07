8d8db2923fd7ad11fd1731d97c9f4cc1f6a1984d
Peter McCracken <peter.mccracken@jadedpixel.com>
2014-12-06 21:25:27 -0500

    prevent changing static strings

--- app/presenters/recipe_presenter.rb
+++ app/presenters/recipe_presenter.rb
@@ -2,7 +2,7 @@ class RecipePresenter < Presenter
   alias_method :recipe, :element
 
   def ingredients_markdown
-    text = recipe.ingredients_text
+    text = recipe.ingredients_text.dup
     recipe_ingredients.each do |ing|
       text.gsub!(ing.text, ing.standard_markdown)
     end
--- test/presenters/recipe_presenter_test.rb
+++ test/presenters/recipe_presenter_test.rb
@@ -5,7 +5,7 @@ class RecipePresenterTest < ActiveSupport::TestCase
   test "#ingredients_markdown links to ingredients" do
     recipe = create(:recipe).tap(&:update_recipe_ingredients)
     p = RecipePresenter.new(recipe)
-    expected =  <<-ING.strip_heredoc
+    expected = <<-ING.strip_heredoc
       1 cup [flour](/ingredients/1-flour)
       [yeast](/ingredients/2-yeast)
       [pizza sauce](/ingredients/3-pizza-sauce)
