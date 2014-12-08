52623dd217a1b2fee2e974cfe1c9cc66e178a6e7
Peter McCracken <peter.mccracken@jadedpixel.com>
2014-12-07 20:08:54 -0500

    fixing presentable

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,7 +2,7 @@ class IngredientsController < AuthenticatedController
   respond_to :html
 
   def show
-    @ingredient = presenter(Ingredient.find(params[:id]))
+    @ingredient = presentable(Ingredient.find(params[:id]))
     respond_with @ingredient
   end
 
