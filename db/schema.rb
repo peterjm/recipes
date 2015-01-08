d3195b0a92fa8d853b9ae32b769334035e302b46
Peter McCracken <peter.mccracken@jadedpixel.com>
2015-01-07 23:28:12 -0500

    adding column to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140413133301) do
+ActiveRecord::Schema.define(version: 20150108042527) do
 
-  create_table "ingredient_names", force: true do |t|
-    t.integer  "ingredient_id", null: false
-    t.string   "name",          null: false
+  create_table "ingredient_names", force: :cascade do |t|
+    t.integer  "ingredient_id",             null: false
+    t.string   "name",          limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20140413133301) do
   add_index "ingredient_names", ["ingredient_id"], name: "index_ingredient_names_on_ingredient_id"
   add_index "ingredient_names", ["name"], name: "index_ingredient_names_on_name"
 
-  create_table "ingredients", force: true do |t|
-    t.string   "name",                         null: false
-    t.boolean  "staple",       default: false, null: false
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name",         limit: 255,                 null: false
+    t.boolean  "staple",                   default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "grams_per_ml"
@@ -33,25 +33,26 @@ ActiveRecord::Schema.define(version: 20140413133301) do
 
   add_index "ingredients", ["name"], name: "index_ingredients_on_name"
 
-  create_table "recipe_ingredients", force: true do |t|
-    t.integer  "recipe_id",                  null: false
-    t.integer  "ingredient_id",              null: false
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer  "recipe_id",                              null: false
+    t.integer  "ingredient_id",                          null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "text",          default: "", null: false
-    t.integer  "position",      default: 0,  null: false
+    t.string   "text",          limit: 255, default: "", null: false
+    t.integer  "position",                  default: 0,  null: false
   end
 
   add_index "recipe_ingredients", ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
   add_index "recipe_ingredients", ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
 
-  create_table "recipes", force: true do |t|
-    t.string   "title",             null: false
+  create_table "recipes", force: :cascade do |t|
+    t.string   "title",             limit: 255, null: false
     t.text     "ingredients_text"
     t.text     "instructions_text"
-    t.string   "source"
+    t.string   "source",            limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
