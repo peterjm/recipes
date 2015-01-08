80dbf851f077815182fb69e10512c234b799a7c0
Peter McCracken <peter.mccracken@jadedpixel.com>
2015-01-07 22:41:08 -0500

    adding uploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,14 @@ class Recipe < ActiveRecord::Base
 
   validates :title, presence: true
 
+  mount_uploader :image, Class.new(Uploader) do
+    version :mini     { process :resize_to_fill => [ 50,  50] }
+    version :thumb    { process :resize_to_fill => [ 80,  80] }
+    version :small    { process :resize_to_fill => [240, 220] }
+    version :medium   { process :resize_to_fill => [350, 240] }
+    version :large    { process :resize_to_fill => [560, 380] }
+  end
+
   include PerformsActionOnSave
   action_on_save :update_recipe_ingredients
 
