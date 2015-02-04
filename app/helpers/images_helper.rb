874a68192593d910735e981da4942e8ce3b52a30
Peter McCracken <peter.mccracken@jadedpixel.com>
2015-02-03 22:12:58 -0500

    delete unused helper method

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,13 +1,5 @@
 module ImagesHelper
 
-  def recipe_image_url(image, size=:large)
-    if image.new_record?
-      image.remote_image_url
-    else
-      image.url(size)
-    end
-  end
-
   def bg_image(url)
     "background-image:url(#{url});background-size:cover"
   end
