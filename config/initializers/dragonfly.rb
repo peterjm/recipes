f906f76ee60dc3c8d07e3ed71f92383258b2206a
Peter McCracken <peter.mccracken@jadedpixel.com>
2015-02-08 15:27:34 -0500

    use dropbox

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -14,16 +14,18 @@ Dragonfly.app.configure do
     /public/
   ]
 
-  datastore :dropbox,
-    app_key:              Rails.application.secrets.dropbox_app_key,
-    app_secret:           Rails.application.secrets.dropbox_app_secret,
-    access_token:         Rails.application.secrets.dropbox_access_token,
-    access_token_secret:  Rails.application.secrets.dropbox_access_token_secret,
-    user_id:              Rails.application.secrets.dropbox_user_id
-
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  if Rails.env.production?
+    datastore :dropbox,
+      app_key:              Rails.application.secrets.dropbox_app_key,
+      app_secret:           Rails.application.secrets.dropbox_app_secret,
+      access_token:         Rails.application.secrets.dropbox_access_token,
+      access_token_secret:  Rails.application.secrets.dropbox_access_token_secret,
+      user_id:              Rails.application.secrets.dropbox_user_id
+  else
+    datastore :file,
+      root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+      server_root: Rails.root.join('public')
+  end
 end
 
 # Logger
