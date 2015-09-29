dc122b730926cc9d7af5d1974091b93ea430c504
Peter McCracken <peter.mccracken@jadedpixel.com>
2015-09-28 22:45:23 -0400

    add ability to create new sources

--- test/controllers/sources_controller_test.rb
+++ test/controllers/sources_controller_test.rb
@@ -20,6 +20,27 @@ class SourcesControllerTest < ActionController::TestCase
     assert_equal "Awesome Food", assigns(:source).name
   end
 
+  test "#new is successful" do
+    get :new
+    assert_response :success
+    assert assigns(:source).new_record?
+  end
+
+  test "#create is successful" do
+    assert_difference "Source.count", 1 do
+      post :create, source: {
+        name: "Awesome Food",
+        source_type: Source::WEBSITE_TYPE,
+        url: "http://awesomefood.com"
+      }
+    end
+    s = Source.last
+    assert_redirected_to source_path(s)
+    assert_equal "Awesome Food", s.name
+    assert_equal "http://awesomefood.com", s.url
+    assert_equal Source::WEBSITE_TYPE, s.source_type
+  end
+
   test "#edit is successful" do
     s = create(:website_source, name: "Awesome Food", url: "http://awesomefood.com")
     get :edit, id: s.id
