23bc4c958c2e54048ca923fb9954174662b74fcc
Peter McCracken <peter@petermccracken.com>
2017-08-11 09:30:16 -0400

    adding disabled state for menu buttons

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,8 +1,9 @@
 module ComponentHelper
 
-  def ui_side_menu_link(text, path, active: false, separated: false, link_params: {})
+  def ui_side_menu_link(text, path, active: false, separated: false, disabled: false, link_params: {})
     li_classes = ['pure-menu-item']
     li_classes << 'pure-menu-selected' if active
+    li_classes << 'pure-menu-disabled' if disabled
     li_classes << 'menu-item-divided' if separated
     content_tag :li, class: li_classes do
       link_to text, path, link_params.merge(class: 'pure-menu-link')
