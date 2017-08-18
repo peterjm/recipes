6cfa1b17f6131ac6eb416e4ef9e81495c2585c72
Peter McCracken <peter@petermccracken.com>
2017-08-17 20:44:50 -0400

    set using postgres to true in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.using_postgres = false
+  config.using_postgres = true
 end
